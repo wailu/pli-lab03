883160de9c61a8bbc1c1fc8461d1f543d46c517b
wailunlim <wailun@u.nus.edu>
2019-01-23 20:21:48 +0800

    fix bug in function 7

--- Lab03.scala
+++ Lab03.scala
@@ -169,7 +169,7 @@ object Lab03 {
       return true
     }
 
-    new Range(2, sqrt(num).toInt, 1).toList.foldLeft (true) ((x, y) => x && num % y != 0)
+    new Range(2, num, 1).toList.foldLeft (true) ((x, y) => x && num % y != 0)
   }
 
   def allPrimes(start:Int, end:Int):List [Int] = {
