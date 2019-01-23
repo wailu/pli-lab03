384721f21c4a26606aa33949d25f44b021032108
wailunlim <wailun@u.nus.edu>
2019-01-23 15:15:09 +0800

    use headoption instead of emulating it

--- Lab03.scala
+++ Lab03.scala
@@ -91,7 +91,7 @@ object Lab03 {
      * 	last_two (List ()) ===> None
      */
 
-    if (xs.takeRight(2).isEmpty) None else Some(xs.takeRight(2).head)
+    xs.takeRight(2).headOption
   }
 
   def compress [A] (xs: List[A]) : List [A] = {
